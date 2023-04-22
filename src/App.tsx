2a6eb398dc92ffb31ec861043f7c45fcf7d27527
donghao666 <1092326322@qq.com>
2023-04-22 15:40:31 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { ChangeEvent, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const synth = window.speechSynthesis;
+  const message = new SpeechSynthesisUtterance();
+
+  const [value, setValue] = useState('')
+
+  const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value)
+  }
+
+  const voicePlay = (text: string) => {
+    message.text = text;
+    message.lang = "zh";
+    message.rate = 1; //语速设置，数字越大越快
+    synth.speak(message);
+  }
+
+  return (
+    <div>
+      <textarea placeholder='请输入文字' value={value} onChange={onChange}></textarea>
+      <button onClick={() => {
+        voicePlay(value)
+      }}>播放</button>
+    </div>
+  )
+}
+
+export default App
